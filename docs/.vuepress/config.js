46623de553b0d25429d57f4ed048b3c507b66058
Joe <lmxgameworld@163.com>
2023-03-05 16:47:16 +0800

    增加功能

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,22 @@
 module.exports = {
+    base: "/docs/",
+    title: "lmxCode",
+    description: "",
     themeConfig: {
         logo: '/assets/img/logo.png',
+        lastUpdated: '更新时间',
+        head: [
+            ['link', { rel: 'icon', content: '/favicon.ico' }],
+            ['meta', { name: 'author', content: '小明coding' }],
+            ['meta', { name: 'keywords', content: 'vuepress介绍,vuepress博客,小明coding' }],
+        ],
         nav: [
             { text: '主页', link: '/' },
             { text: '关于', link: '/about/' },
             { text: '主页面about', link: '/about' },
-            { text: 'External', link: 'https://google.com' },
+            { text: '谷歌', link: 'https://google.com' },
             {
-                text: 'Languages',
+                text: '语言（未配置）',
                 ariaLabel: 'Language Menu',
                 items: [
                     { text: 'Chinese', link: '/language/chinese/' },
@@ -23,8 +32,8 @@ module.exports = {
         ],
         sidebar: [
             '/',
-            '/page-a',
-            ['/page-b', 'Explicit link text']
-          ]
+            '/about',
+            ['/about/', 'Explicit link text']
+        ]
     }
 }
